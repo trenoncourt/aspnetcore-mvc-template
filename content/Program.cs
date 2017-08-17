fb574a75583f04bbd330fbd4f0bb3ba1e0ae8f4f
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-08-17 11:24:56 +0200

    Add simple project init

---
+++ content/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreApi
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BuildWebHost().Run();
+        }
+
+        public static IWebHost BuildWebHost() =>
+            new WebHostBuilder()
+                .UseKestrel()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                })
+                .Build();
+    }
+}
---
+++ content/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace AspNetCoreApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+            .AddCors()
+            .AddMvcCore()
+                .AddJsonFormatters(settings =>
+                {
+                    settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    settings.NullValueHandling = NullValueHandling.Ignore;
+                    settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    settings.DefaultValueHandling = DefaultValueHandling.Ignore;
+                    settings.Formatting = Formatting.None;
+                });
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {            
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseMvc();
+        }
+    }
+}
